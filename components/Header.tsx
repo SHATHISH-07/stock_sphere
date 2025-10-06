0192a2c68d53f7a911587ffcb443b59cbac964a6
Shathish2004 <shathishkumaran07@gmail.com>
2025-10-06 18:13:04 +0530

    Completed the Header

---
+++ components/Header.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import Link from "next/link";
+import logo from "../public/assets/icons/logo.svg";
+import NavItems from "./NavItems";
+import UserDropDown from "./UserDropDown";
+
+const Header = () => {
+  return (
+    <header className="sticky top-0 header">
+      <div className="container header-wrapper">
+        <Link href="/">
+          <Image
+            src={logo}
+            alt="logo"
+            width={140}
+            height={32}
+            className="h-8 w-auto cursor-pointer"
+          ></Image>
+        </Link>
+        <nav className="hidden sm:block">
+          <NavItems />
+        </nav>
+        <UserDropDown />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
